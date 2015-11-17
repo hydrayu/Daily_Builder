1406a23e95f029927f78b3eaf6dc7dc459388bc0
Po_Hsun <showoowohs@google.com>
2015-11-17 18:17:05 +0800

    add Hydra_DailyBuild_MT8382_PHONE.sh , modified:   README.md

---
+++ Hydra_DailyBuild_MT8382_PHONE.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DES=~/SourceCode/MT8382_PHONE
+SRC=~/SourceCode
+DATE=$(date +%m%d%H%M%S)
+CDATE=$(date +%Y_%m%d_%H%M%S)
+DESFILE=status_"$DATE".log
+
+find $DES -type d -mmin -780 | grep -v '^.*/.git/.*' | grep  '^.*/.git$' > ~/$DESFILE
+
+if [ ! -d $SRC/Daily_build ]; then
+	mkdir  $SRC/Daily_build
+fi
+if [ ! -d $SRC/Daily_build/MT8382_PHONE ]; then
+	mkdir $SRC/Daily_build/MT8382_PHONE
+fi
+
+DB="$SRC"/Daily_build/MT8382_PHONE
+
+if [ -e ~/$DESFILE ] && [ -s ~/$DESFILE ];then
+	cd $DES
+	BRANCH=$(git branch | grep '^*' | cut -d ' ' -f 2)
+	git checkout mediatek/kernel/trace32/mt82kk_tb_ramdisk.img 
+	git status | grep '^#.*modified:' | sed 's/^#.*modified: / git add/g' > add.sh
+	git status | grep '^#.*deleted:' | sed 's/^#.*deleted: / git rm/g' >> add.sh
+	chmod 777 -R add.sh
+	`./add.sh`
+	git commit -m "Daily commit "$CDATE" "	
+	CMNUM=$(git log | grep '^commit' | head -n 1 | cut -d ' ' -f 2 | cut -c 1-7)
+	"$DES"/makeMtk mt82kk_tb n;"$DES"/makeMtk mt82kk_tb systemimage;"$DES"/makeMtk mt82kk_tb otapackage;	
+	mkdir "$DB"/MT8382_PHONE_"$DATE"_"$BRANCH"_"$CMNUM" 
+	echo yes | ~/bin/cp_MTK_pandora_img out/target/product/mt82kk_tb "$DB"/MT8382_PHONE_"$DATE"_"$BRANCH"_"$CMNUM"
+	rm -rf ~/"$DESFILE" ./add.sh
+else 
+	echo "No Changes, Daily Build PASS"
+	echo "No Changes, Daily Build PASS" > "$DB"/"$CDATE"_PASS.log
+	rm -rf ~/"$DESFILE"
+fi
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Daily_Builder
+# 2015/11/17 18:15
+# add Hydra_DailyBuild_MT8382_PHONE.sh
+# but not test yet
