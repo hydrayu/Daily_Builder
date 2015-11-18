d23abbc721cca794b4eb3239acf03b1d7ca96d36
hydrayu <g800805@gmail.com>
2015-11-18 10:34:04 +0800

    modified:   Hydra_DailyBuild_8960.sh , add echo log , modify bug

--- Hydra_DailyBuild_8960.sh
+++ Hydra_DailyBuild_8960.sh
@@ -7,16 +7,20 @@ CDATE=$(date +%Y_%m%d_%H%M%S)
 DESFILE=status_"$DATE".log
 
 find $DES -type d -mmin -780 | grep -v '^.*/.git/.*' | grep  '^.*/.git$' > ~/$DESFILE
-
+echo " Checkinging Daily Build Directory..."
 if [ ! -d $SRC/Daily_build ]; then
+	echo " Building Daily Build Directory..."
 	mkdir $SRC/Daily_build
 fi
 if [ ! -d $SRC/Daily_build/8960 ]; then
+	echo " Building Daily Build 8960 Directory..."
 	mkdir $SRC/Daily_build/8960
 fi
 
 DB="$SRC"/Daily_build/8960
+echo " Checkinging .git Status"
 if [ -e ~/$DESFILE ] && [ -s ~/$DESFILE ];then
+        echo " Start Daily Commit and Building !!!"
 	cd $DES
 	BRANCH=$(git branch | grep '^*' | cut -d ' ' -f 2)
  
@@ -28,9 +32,12 @@ if [ -e ~/$DESFILE ] && [ -s ~/$DESFILE ];then
 	CMNUM=$(git log | grep '^commit' | head -n 1 | cut -d ' ' -f 2 | cut -c 1-7)
 	rm -rf out/
 	$DES/wade.sh n;$DES/wade.sh otapackage;	
+	echo " Building Finish !!! "
+	echo " Copying Image..."
 	mkdir $DB/8960_"$DATE"_"$BRANCH"_"$CMNUM" 
-	echo yes | ~/bin/cp_IQ8_img out/debug/target/product/msm8960 $DB/8961_"$DATE"_"$BRANCH"_"$CMNUM"
+	echo yes | ~/bin/cp_IQ8_img out/debug/target/product/msm8960 $DB/8960_"$DATE"_"$BRANCH"_"$CMNUM"
 	rm -rf ~/$DESFILE ./add.sh
+	echo "Done !!!"
 else    
         echo "No Changes, Daily Build PASS"
 	echo "No Changes, Daily Build PASS" > "$DB"/"$CDATE"_PASS.log
