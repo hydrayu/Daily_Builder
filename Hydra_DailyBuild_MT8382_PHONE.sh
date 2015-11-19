60c2be56e5c8505acca795c347648907314ca683
hydrayu <g800805@gmail.com>
2015-11-19 11:56:33 +0800

    修改了判斷方式,當日沒有做新的commit才會執行

--- Hydra_DailyBuild_MT8382_PHONE.sh
+++ Hydra_DailyBuild_MT8382_PHONE.sh
@@ -4,9 +4,15 @@ DES=~/SourceCode/MT8382_PHONE
 SRC=~/SourceCode
 DATE=$(date +%m%d%H%M%S)
 CDATE=$(date +%Y_%m%d_%H%M%S)
-DESFILE=status_"$DATE".log
+NDATE=$(date +"%d %b %Y")
 
-find $DES -type d -mmin -780 | grep -v '^.*/.git/.*' | grep  '^.*/.git$' > ~/$DESFILE
+cd $DES
+CMDATE=$(git log -1 --format=%cD | cut -d ' ' -f 2-4)
+
+#DESFILE=status_"$DATE".log
+#find $DES -type d -mmin -780 | grep -v '^.*/.git/.*' | grep  '^.*/.git$' > ~/$DESFILE
+
+
 echo " Checkinging Daily Build Directory..."
 if [ ! -d $SRC/Daily_build ]; then
 	echo " Building Daily Build Directory..."
@@ -19,7 +25,7 @@ fi
 
 DB="$SRC"/Daily_build/MT8382_PHONE
 echo " Checkinging .git Status"
-if [ -e ~/$DESFILE ] && [ -s ~/$DESFILE ];then
+if [ "$CMDATE" != "$NDATE" ];then
 	echo " Start Daily Commit and Building !!!"
 	cd $DES
 	BRANCH=$(git branch | grep '^*' | cut -d ' ' -f 2)
@@ -29,16 +35,16 @@ if [ -e ~/$DESFILE ] && [ -s ~/$DESFILE ];then
 	chmod 777 -R add.sh
 	`./add.sh`
 	git commit -m "Daily commit "$CDATE" "	
-	CMNUM=$(git log | grep '^commit' | head -n 1 | cut -d ' ' -f 2 | cut -c 1-7)
+	CMNUM=$(git log --pretty=oneline --abbrev-commit | head -n 1 | cut -d ' ' -f 1)
 	"$DES"/makeMtk mt82kk_tb n;"$DES"/makeMtk mt82kk_tb systemimage;"$DES"/makeMtk mt82kk_tb otapackage;	
 	echo " Building Finish !!! "
 	echo " Copying Image..."
 	mkdir "$DB"/MT8382_PHONE_"$DATE"_"$BRANCH"_"$CMNUM" 
 	echo yes | ~/bin/cp_MTK_pandora_img out/target/product/mt82kk_tb "$DB"/MT8382_PHONE_"$DATE"_"$BRANCH"_"$CMNUM"
-	rm -rf ~/"$DESFILE" ./add.sh
+	rm -rf ./add.sh
 	echo "Done !!!"
 else 
-	echo "No Changes, Daily Build PASS"
-	echo "No Changes, Daily Build PASS" > "$DB"/"$CDATE"_PASS.log
-	rm -rf ~/"$DESFILE"
+	echo "Already Commit Today , Daily Build PASS"
+	echo "Already Commit Today , Daily Build PASS" > "$DB"/"$CDATE"_PASS.log
+	
 fi
